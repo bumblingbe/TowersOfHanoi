8b77351f0ad3a0ff72482f3563a0a35218208b2b
Bethia Blond <79747651+bumblingbe@users.noreply.github.com>
2024-06-07 12:33:05 +0800

    Added input validation

--- towersOfHanoi.py
+++ towersOfHanoi.py
@@ -1,6 +1,8 @@
-# define function for n blocks, starting on rod 'start' and ending on rod 'end'.
-# assumptions: n is positive, three rods numbered 1, 2 and 3.
+!pip install PyInputPlus
+import pyinputplus as pyip
 
+# define function for n blocks, starting on rod 'start' and ending on rod 'end'.
+# assumptions: n is positive, three rods (numbered 1, 2 and 3).
 def hanoi(n,start,end):
     '''
     Prints step-by-step instructions for solving Towers of Hanoi problem 
@@ -8,7 +10,25 @@ def hanoi(n,start,end):
     The 'start' and 'end' rod arguments must be positive integers.
     Assumes 'n' is positive and there are 3 rods (rod 1, rod 2 and rod 3).
     '''
-    
+
+    # validate parameters
+    def checkPosInt(n,start,end):
+        for arg in [n,start,end]:
+            if arg%1 != 0 or arg<=0:
+                return 'invalid'
+    if checkPosInt(n,start,end)== 'invalid':
+        print("Error: all arguments must be positive whole numbers greater than zero.")
+        return
+
+    if start == end:
+        print("'start' must be different to 'end'")
+        return
+
+    for pos in [start, end]:
+        if pos not in [1,2,3]:
+            print("'start' and 'end' must be 1, 2, or 3")
+
+    # print instructions themselves
     if n == 1:
         # base case: only 1 block
         print(f'move top block from {start} to {end}')

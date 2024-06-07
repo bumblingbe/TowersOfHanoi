119cb68ae57db5b044de17400e966f0340327a23
Bethia Blond <79747651+bumblingbe@users.noreply.github.com>
2024-06-07 10:38:53 +0800

    Create towersOfHanoi.py
    initial attempt

---
+++ towersOfHanoi.py
@@ -0,0 +1,19 @@
+def hanoi(n,start,end):
+    if n == 1:
+        print(f'move top block from {start} to {end}')
+        return
+    else:
+        #declare 'middle' rod option
+        rodOptions = [1,2,3]
+        rodOptions.pop(start)
+        rodOptions.pop(end)
+        middle = rodOptions[0]
+        
+        #move all-but-base to middle
+        hanoi(n-1,start,middle)
+
+        #move base to end
+        print(f'move top block from {start} to {end}')
+
+        #move all-but-base to end
+        hanoi(n-1,middle,end)
